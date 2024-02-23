ae95f0492a95708800f2d9d335af0ef11e374737
Vlad Erma <ssvetlaa@235gmail.com>
2024-02-24 00:20:18 +0300

    finish task-5

--- src/test/java/ru/erma/config/AbstractTestContainerConfig.java
+++ src/test/java/ru/erma/config/AbstractTestContainerConfig.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.test.context.TestPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
